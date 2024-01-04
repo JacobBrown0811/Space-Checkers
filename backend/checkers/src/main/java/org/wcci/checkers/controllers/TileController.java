a5ef826657f05a3e2edf39ef90ac8f3dc3260167
Jacob Brown <jacob.moorshujaa@gmail.com>
2024-01-04 20:20:50 +0100

    review tile controller

--- backend/checkers/src/main/java/org/wcci/checkers/controllers/TileController.java
+++ backend/checkers/src/main/java/org/wcci/checkers/controllers/TileController.java
@@ -1,9 +1,15 @@
 package org.wcci.checkers.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.wcci.checkers.models.TileModel;
 import org.wcci.checkers.repositories.TileRepository;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/tiles")
@@ -18,8 +24,16 @@ public class TileController {
     }
 
     @GetMapping("/{id}")
-    public TileModel getTile(@PathVariable long id) {
-        return tileRepository.findById(id).orElse(null);
+    public ResponseEntity<TileModel> getTile(@PathVariable long id) {
+        Optional<TileModel> tile = tileRepository.findById(id);
+        return tile.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<TileModel> updateTile(@PathVariable long id, @RequestBody TileModel updatedTile) {
+         tileRepository.findById(id)
+         .map(updatedTile -> {
+            
+        
     }
 
 
