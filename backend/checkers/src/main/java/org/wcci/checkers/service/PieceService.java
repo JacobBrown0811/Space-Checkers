b3e2fdfc715b3e7dfce59d9fe17e1cc5e026e14d
Josh Wilks <Joshua.S.Wilks@Gmail.com>
2024-01-16 09:46:25 -0700

    piece service booleans updated

--- backend/checkers/src/main/java/org/wcci/checkers/service/PieceService.java
+++ backend/checkers/src/main/java/org/wcci/checkers/service/PieceService.java
@@ -22,7 +22,6 @@ public class PieceService {
 
     /**
      * set piece location on move.
-     * TODO Needs update to use tile ID instead of row, column
      * 
      * @param pieceId
      * @param newRow
@@ -32,53 +31,99 @@ public class PieceService {
     public boolean movePiece(long pieceId, long newTileId) {
         Optional<PieceModel> pieceOpt = pieceRepository.findById(pieceId);
         Optional<TileModel> tileOpt = tileRepository.findById(newTileId);
-        
-        if (pieceOpt.isPresent()) {
-            try {
-            PieceModel piece = pieceOpt.get();
-            TileModel newTile = tileOpt.get();
-            TileModel oldTile = piece.getTile();
-            oldTile.setIsOccupied(false);
-            piece.setBoardRow(newTile.getBoardRow());
-            piece.setBoardColumn(newTile.getBoardColumn());
-            newTile.setIsOccupied(true);
-            tileRepository.save(oldTile);
-            tileRepository.save(newTile);
-            pieceRepository.save(piece);
-            return true;
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println("No can do bud: " + e.getMessage());
-            return false;
-        }
-        }
-        return false;
-    }
 
-    public boolean canMove(long pieceId) {
-        Optional<PieceModel> pieceOpt = pieceRepository.findById(pieceId);
         if (pieceOpt.isPresent()) {
             try {
                 PieceModel piece = pieceOpt.get();
-                int pRow = piece.getBoardRow();
-                int pCol = piece.getBoardColumn();
-                long tileId = piece.getTile().getId();
-                    long trTile = tileId - 7;
-                    long tlTile = tileId - 9;
-                if (piece.isKing()) {
-                    long brTile = tileId + 9;
-                    long blTile = tileId + 7;
-                } else {                  
-                    if ()
-                }
+                TileModel newTile = tileOpt.get();
+                TileModel oldTile = piece.getTile();
+                oldTile.setIsOccupied(false);
+                piece.setBoardRow(newTile.getBoardRow());
+                piece.setBoardColumn(newTile.getBoardColumn());
+                newTile.setIsOccupied(true);
+                tileRepository.save(oldTile);
+                tileRepository.save(newTile);
+                pieceRepository.save(piece);
+                return true;
             } catch (Exception e) {
                 // TODO: handle exception
+                System.out.println("Problem with movePiece: " + e.getMessage());
+                return false;
             }
         }
         return false;
     }
 
-    
+    /**
+     * Returns true if piece can move
+     * @param pieceId
+     * @return
+     */
+    public boolean canMove(long pieceId) {
+        Optional<PieceModel> pieceOpt = pieceRepository.findById(pieceId);
+        if (pieceOpt.isPresent()) {
+            try {
+                PieceModel piece = pieceOpt.get();
+                long tileId = piece.getTile().getId();
+                long trTile = tileId - 7;
+                long tlTile = tileId - 9;
+                Optional<TileModel> trOpt = tileRepository.findById(trTile);
+                Optional<TileModel> tlOpt = tileRepository.findById(tlTile);
+                if (piece.isKing()) {
+                    long brTile = tileId + 9;
+                    long blTile = tileId + 7;
+                    Optional<TileModel> brOpt = tileRepository.findById(brTile);
+                    Optional<TileModel> blOpt = tileRepository.findById(blTile);
+                    if (blOpt.get().getIsOccupied() && brOpt.get().getIsOccupied() && trOpt.get().getIsOccupied()
+                            && tlOpt.get().getIsOccupied()) {
+                        return false;
+                    }
+                } else {
+
+                    if (trOpt.get().getIsOccupied() && tlOpt.get().getIsOccupied()) {
+                        return false;
+                    }
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println("Problem with canMove: " + e.getMessage());
+            }
+        }
+        return true;
+    }
+
+    public boolean canCapture(long pieceId) {
+        Optional<PieceModel> pieceOpt = pieceRepository.findById(pieceId);
+        PieceModel piece = pieceOpt.get();
+        try {
+            if (!canMove(pieceId)){
+                long tileId = piece.getTile().getId();
+                long trTile = tileId - 14;
+                long tlTile = tileId - 18;
+                Optional<TileModel> trOpt = tileRepository.findById(trTile);
+                Optional<TileModel> tlOpt = tileRepository.findById(tlTile);
+                if (piece.isKing()) {
+                    long brTile = tileId + 18;
+                    long blTile = tileId + 14;
+                    Optional<TileModel> brOpt = tileRepository.findById(brTile);
+                    Optional<TileModel> blOpt = tileRepository.findById(blTile);
+                    if (blOpt.get().getIsOccupied() && brOpt.get().getIsOccupied() && trOpt.get().getIsOccupied()
+                            && tlOpt.get().getIsOccupied()) {
+                        return false;
+                    }
+                } else {
+
+                    if (trOpt.get().getIsOccupied() && tlOpt.get().getIsOccupied()) {
+                        return false;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Problem with canCapture: " + e.getMessage());
+        }
+        return true;
+    }
 
     /**
      * delete piece on capture
@@ -88,8 +133,9 @@ public class PieceService {
      */
     public boolean capturePiece(long pieceId) {
         Optional<PieceModel> pieceOpt = pieceRepository.findById(pieceId);
+
         if (pieceOpt.isPresent()) {
-            pieceRepository.delete(pieceOpt.get());
+            pieceRepository.deleteById(pieceId);
             return true;
         }
         return false;
