bd1db6f2c65297a213a4da6264bf501da1dc9fa0
Ryan Boney <ryanboney@gmail.com>
2024-01-22 12:34:51 -0500

    about / rules complete

--- frontend/checker/src/components/Team.jsx
+++ frontend/checker/src/components/Team.jsx
@@ -6,6 +6,16 @@ const Team = () => {
     <>
     <a href="/">
     <article className="team">
+
+    <div className="member" id="jacob">
+        <img
+          src="./src/assets/images/jacob.jpg"
+          alt="Probably the same guy"
+        />
+        <div className="bio">
+        Jacob is a bright and ambitious team leader. This project has not only sharpened his technical skills but has also instilled a deep sense of accomplishment as a junior developer. It reminds me that with determination and teamwork, we can conquer any coding challenge that comes our way.</div>
+      </div>
+
       <div className="member" id="ryan">
         <img
           src="./src/assets/images/ryan.jpg"
@@ -21,15 +31,6 @@ const Team = () => {
         <div className="bio">
         Ronald is an established business owner and a Java developer, combining expertise in home improvement and web development to offer high-quality services to clients.  He is on a journey through the world of software design to transition into a full-time developer role while concurrently managing his business; Ron is a true master of multi-tasking.
         </div>
-      </div>
-
-      <div className="member" id="jacob">
-        <img
-          src="./src/assets/images/jacob.jpg"
-          alt="Probably the same guy"
-        />
-        <div className="bio">
-        Laissez le bon temps rouler!</div>
       </div>
 
       <div className="member" id="josh">
