52012ceaedf62fb9c5edc349099fe68460e36875
Ryan Boney <ryanboney@gmail.com>
2024-01-22 20:36:09 -0500

    font readability upgrade

--- frontend/checker/src/components/Rules.jsx
+++ frontend/checker/src/components/Rules.jsx
@@ -10,7 +10,7 @@ return(
         <h2>Movement:</h2>Pieces traverse the cosmic expanse by moving diagonally forward one space, representing their intergalactic journey.
     A piece achieves the esteemed title of "Star Sovereign" if it reaches the last row of the opponent's cosmic quadrant, gaining the ability to move both forward and backward across the cosmic battlefield.
         <h2>Capture:</h2>Engage in cosmic conquest by capturing an opponent's piece. Leapfrog over an adversary's celestial entity when an empty space beckons beyond, securing victory in the name of your galactic empire. Be careful, traversal through the nebulas has disrupted IFF tracking! Friendly-fire is still possible!
-    Coming Soon - Multi-Warp DLC: The cosmos applauds multiple captures in a single turn, allowing for a cascade of strategic moves in a continuous diagonal line or sucessive pattern.
+    <br/><br/><b>Coming Soon - Multi-Warp DLC:</b> The cosmos applauds multiple captures in a single turn, allowing for a cascade of strategic moves in a continuous diagonal line or sucessive pattern.
     <h2>Star Sovereign's Capture:</h2>Ascendant Star Sovereigns, now capable of traversing the cosmos with unparalleled versatility, can capture opponents by jumping over their celestial counterparts diagonally forward or backward.
     <h2>End of the Celestial Clash:</h2>The intergalactic conflict concludes when one player seizes control of the entire cosmic domain, effectively capturing all of their opponent's astral entities or confining them to a celestial blockade.
     Alternatively, a draw in the cosmic symphony occurs if the cosmic constellations align, repeating the same celestial position three times, or if both entities are trapped in a cosmic stalemate with no legal moves.
